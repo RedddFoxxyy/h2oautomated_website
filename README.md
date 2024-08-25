2015e1e81b35f65c5ffbc93cd5737fe89a722ca3
RedddFoxxyy <127536688+RedddFoxxyy@users.noreply.github.com>
2024-08-25 16:36:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# h2oautomated_website
+ The website to control the autonomous water surface cleaning robot.
---
+++ robo_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoboAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'robo_app'
