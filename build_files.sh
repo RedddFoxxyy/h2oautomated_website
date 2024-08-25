d5c3bee66737c663b5b062c11094d554a1c4b752
RedddFoxxyy <127536688+RedddFoxxyy@users.noreply.github.com>
2024-08-25 22:59:22 +0530

    re

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,10 @@
 # build_files.sh
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
+python3.12 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
 
+python3.12 -m pip install --upgrade pip
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3.12 manage.py collectstatic --noinput
